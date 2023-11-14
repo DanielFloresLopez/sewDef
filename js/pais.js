ac4abb1938b532e5bce5e862dd316573f849eb55
DanielFloresLopez <113984512+DanielFloresLopez@users.noreply.github.com>
2023-11-14 12:52:41 +0100

    Add files via upload

---
+++ js/pais.js
@@ -0,0 +1,31 @@
+"use strict";
+class Pais{
+    constructor(nombre, capital, poblacion, formaGobierno){
+        this.nombre = nombre;
+        this.capital = capital;
+        this.poblacion = poblacion;
+        this.formaGobierno = formaGobierno;
+    }
+
+    fillAttributes(latitudCap, longitudCap, religion){
+        this.latitudCap = latitudCap;
+        this.longitudCap = longitudCap;
+        this.religion = religion;
+    }
+    getNombre(){
+        return this.nombre;
+    }
+    getCapital(){
+        return this.capital;
+    }
+
+    getSecondaryInfoAsAList(){
+        return "<ul><li>"+this.poblacion+"</li><li>"
+        +this.formaGobierno+
+        "</li><li>"+this.religion+
+        "</li></ul>";
+    }
+    writeCapital(){
+        document.write("<p> Coordenadas de "+this.capital+": latitud{" +this.latitudCap+"}, longitud{"+this.longitudCap+"}</p>");
+    }
+}
